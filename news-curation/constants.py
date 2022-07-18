4227bca353319e070169b0f4d377abd053582ddc
Nikit Periwal <nikit.periwal@gmail.com>
2022-07-18 21:28:15 +0530

    replaced reading from .env

--- news-curation/constants.py
+++ news-curation/constants.py
@@ -1,12 +1,13 @@
-from secret_keys import mongo_username, mongo_password, api_key
+import os
 import pytz
+from dotenv import load_dotenv
 
 # Defining the constants to be used throughout the app
 DATETIME_TO_STRING = "%m/%d/%Y, %H:%M:%S"
 UTC_OFFSET = pytz.timezone('Asia/Kolkata')._utcoffset
 
 # News Scraper
-NEWS_API_KEY = api_key
+NEWS_API_KEY = os.environ["API_KEY"]
 
 LANGUAGE = "en"
 COUNTRY = "in"
@@ -19,6 +20,8 @@ SPLIT_REGEX = r"\w+(?:'\w+)?|[^\w\s]"
 SCHEDULE_MINUTES = 60
 
 # MongoDB Constants
+mongo_username = os.environ["MONGO_USERNAME"]
+mongo_password = os.environ["MONGO_PASSWORD"]
 MONGO_URL = f"mongodb+srv://{mongo_username}:{mongo_password}@shortly.autde.mongodb.net/?retryWrites=true&w=majority"
 MONGO_DB_NAME = "news_db"
 CURATED_TABLE = "curated_news"
